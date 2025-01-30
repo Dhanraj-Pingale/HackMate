63bd8d02c0352833e0485ed5e0b0487aba3da4fd
Dhanraj Pingale <pingaledhanraj15@gmail.com>
2025-01-30 12:49:09 +0530

    added updatStudent

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  teckStack: {type: String, required: false},
+  techStack: {type: String, required: false},
   bio: {type: String, required: false},
   linkedIn: {type: String, required: false},
   github: {type: String, required: false},
